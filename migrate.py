7071c767c25807fef78034ae9861c056083a30b8
Terry_Liu <tkuterry.liu@gmail.com>
2023-03-05 15:19:18 +0800

    Complete depth tracking

--- migrate.py
+++ migrate.py
@@ -1,4 +1,4 @@
-import re, os, argparse
+import re, os, argparse, shutil
 
 RE_BEAR_IMAGE_UUID = "[0-9A-Z]{8}\-[0-9A-Z]{4}\-[0-9A-Z]{4}\-[0-9A-Z]{4}\-[0-9A-Z]{12}\.[a-z]+"
 TEST = 'Abstract%20Factory/90489A74-398D-411C-A281-52D6B16F6009.png'
@@ -15,13 +15,19 @@ def main():
     args = process_args()
     prefix = args.rootDir.rsplit('/', 1)[0]
     print(prefix)
-    depth = 1
-    depth_map = {}
+    stack = []
+
     for currentFolder, subfolders, filenames in os.walk(args.rootDir):
-        print('depth:', depth)
         print('Current folder: ' + currentFolder)
         print('Subfolders: ' + str(subfolders))
         # print('files: ' + str(filenames))
+
+        stack.append(len(subfolders))
+
+        depth = len(stack)
+        print("depth: " + str(depth))
+        print(stack)
+
         mdList = [name.removesuffix('.md')
                   for name in filenames if name.endswith('.md')]
         pngList = [name
@@ -29,47 +35,20 @@ def main():
 
         if len(mdList) > 0:
             # create the target folder
-            target_folder = currentFolder.rsplit('/', depth)[-1]
+            target_folder = '/'.join(currentFolder.rsplit('/', depth)[-depth:])
             print(currentFolder.rsplit('/', depth))
+            os.mkdir(target_folder)
             # copy md files to the target folder
             for f in filenames:
-                print('copy ' + '/'.join([target_folder, f]))
+                print('copy ' + currentFolder + \
+                      '/' + f + ' to ' + target_folder + '/' + f)
 
-        if len(subfolders) > 0:
-            depth = depth + 1
-            depth_map[depth] = len(subfolders)
-        else:
-            depth_map[depth] = depth_map[depth] - 1
-        print('left folders: ', depth_map[depth])
-        if depth_map[depth] == 0:
-            depth = depth - 1
+        while len(stack) > 1 and stack[-1] == 0:
+            stack.pop()
+            stack[-1] = stack[-1] - 1
 
         print('--------------------------------')
 
-    
-    
-    # for folderPath, subfolders, filenames in os.walk(args.rootDir):
-    #     print('Current folder: ' + folderPath)
-    #     print('Subfolders: ' + str(subfolders))
-    #     print('files: ' + str(filenames))
-        # mdList = [name.removesuffix('.md')
-        #           for name in filenames if name.endswith('.md')]
-        # pngList = [name
-        #           for name in filenames if name.endswith('.png')]
-        # print(str(mdList))
-        # print(str(pngList))
-        # if len(mdList) > 0:
-        #     # create folder for this one
-        #     print(folderPath.rsplit('/')[-1])
-
-        # print('--------------------------------')
-
-
-        # for fullName in filenames:
-        #     if fullName.endswith('.md'):
-        #         name = fullName.rstrip('.md')
-        #         print(name)
-    
     # print(TEST)
     # match = re.search(RE_BEAR_IMAGE_UUID, TEST)
     # print(f'![[{match.group()}]]')
